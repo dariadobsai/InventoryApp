599ec71ea694bd840497ec49d419d6bef6664c7a
DariaKalashnik <dashavinni0@gmail.com>
2017-10-11 00:24:49 +0300

    Add Contextual Action Bar in Catalog Activity

--- app/src/main/java/com/example/android/shop/CatalogActivity.java
+++ app/src/main/java/com/example/android/shop/CatalogActivity.java
@@ -11,13 +11,20 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.android.shop.data.ProductContract.ShopEntry;
+
+import java.util.HashMap;
+import java.util.List;
 
 // Created by Daria Kalashnikova 11.07.2017
 
@@ -30,10 +37,14 @@ public class CatalogActivity extends AppCompatActivity implements LoaderCallback
     Intent intent;
     private Uri mCurrentUri;
 
+    private SelectionAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        mAdapter = new SelectionAdapter(this, R.layout.list_item, R.id.construction, productList);
 
         // Setup FAB to open EditorActivity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -56,6 +67,65 @@ public class CatalogActivity extends AppCompatActivity implements LoaderCallback
         mCursorAdapter = new ProductAdapter(this, null);
         productList.setAdapter(mCursorAdapter);
 
+        productList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+
+        productList.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+            private int itemsSelected = 0;
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                mAdapter.clearSelection();
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                itemsSelected = 0;
+                MenuInflater inflater = getMenuInflater();
+                inflater.inflate(R.menu.menu_catalog, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_delete_all_entries:
+                        deleteAllProducts();
+                        itemsSelected = 0;
+                        mAdapter.clearSelection();
+                        mode.finish();
+                }
+                return false;
+            }
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                if (checked) {
+                    itemsSelected++;
+                    mAdapter.setNewSelection(position, checked);
+                } else {
+                    itemsSelected--;
+                    deleteAllProducts();
+                    mAdapter.removeSelection(position);
+                }
+                mode.setTitle(itemsSelected + " selected");
+
+            }
+        });
+
+        productList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+                productList.setItemChecked(position, !mAdapter.isPositionChecked(position));
+                return false;
+            }
+        });
+
         productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
@@ -73,24 +143,6 @@ public class CatalogActivity extends AppCompatActivity implements LoaderCallback
     private void deleteAllProducts() {
         int rowsDeleted = getContentResolver().delete(ShopEntry.CONTENT_URI, null, null);
         Log.v("CatalogActivity", rowsDeleted + " rows deleted from shop database");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_catalog.xml file
-        getMenuInflater().inflate(R.menu.menu_catalog, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            // Respond to a click on the "Delete all entries" menu option
-            case R.id.action_delete_all_entries:
-                deleteAllProducts();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -123,5 +175,49 @@ public class CatalogActivity extends AppCompatActivity implements LoaderCallback
     public void onLoaderReset(Loader<Cursor> loader) {
         // Callback called when the data needs to be deleted
         mCursorAdapter.swapCursor(null);
+    }
+
+    private class SelectionAdapter extends ArrayAdapter<ListView> {
+
+        private HashMap<Integer, Boolean> mSelection = new HashMap<>();
+
+        public SelectionAdapter(CatalogActivity act, int resource, int construction, ListView list) {
+            super(act, resource, construction, (List<ListView>) list);
+        }
+
+        public void setNewSelection(int position, boolean value) {
+            mSelection.put(position, value);
+            mCursorAdapter.notifyDataSetChanged();
+        }
+
+        public boolean isPositionChecked(int position) {
+            Boolean result = mSelection.get(position);
+            return result == null ? false : result;
+        }
+
+//        public Set<Integer> getCurrentCheckedPosition() {
+//            return mSelection.keySet();
+//        }
+
+        public void removeSelection(int position) {
+            mSelection.remove(position);
+            mCursorAdapter.notifyDataSetChanged();
+        }
+
+        public void clearSelection() {
+            mSelection = new HashMap<>();
+            mCursorAdapter.notifyDataSetChanged();
+        }
+
+//        @NonNull
+//        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+//            View v = mAdapter.getView(position, convertView, parent);//let the adapter handle setting up the row views
+//            v.setBackgroundColor(getResources().getColor(android.R.color.background_light)); //default color
+//
+//            if (mSelection.get(position) != null) {
+//                v.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));// this is a selected position so make it red
+//            }
+//            return v;
+//        }
     }
 }
--- app/src/main/java/com/example/android/shop/ProductAdapter.java
+++ app/src/main/java/com/example/android/shop/ProductAdapter.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.android.shop.data.ProductContract.ShopEntry;
 
